6a4f8a15cd09a6535c31370e75b5cd2af709f195
kdomo <dongho5088@naver.com>
2022-11-07 18:09:08 +0900

    init

---
+++ src/main/java/com/domo/mysqlreplication/MysqlReplicationApplication.java
@@ -0,0 +1,13 @@
+package com.domo.mysqlreplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlReplicationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MysqlReplicationApplication.class, args);
+    }
+
+}
